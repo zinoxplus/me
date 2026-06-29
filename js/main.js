b679dbf7539a8fff97989c779e42c493b649499c
ZINOX <mahdiyarmoradi93@gmail.com>
2026-06-29 12:27:28 +0330

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('progress').onclick=(e)=>{
- const rect=e.currentTarget.getBoundingClientRect();
- const p=(e.clientX-rect.left)/rect.width;
- audio.currentTime=p*audio.duration;
-};
